3c9101f92eadbd8df80384ac35760fbf9ec5d022
yxc407 <yuxiuchen407@gmail.com>
2025-01-15 16:50:50 +0800

    Integrate strongsort and ROS

--- strong_sort/sort/detection.py
+++ strong_sort/sort/detection.py
@@ -29,7 +29,8 @@ class Detection(object):
     def __init__(self, tlwh, confidence, feature):
         self.tlwh = np.asarray(tlwh, dtype=float)
         self.confidence = float(confidence)
-        self.feature = np.asarray(feature.cpu(), dtype=np.float32)
+        # self.feature = np.asarray(feature.cpu(), dtype=np.float32)
+        self.feature = feature.detach().cpu().numpy().astype(np.float32)
 
     def to_tlbr(self):
         """Convert bounding box to format `(min x, min y, max x, max y)`, i.e.,
--- strong_sort/strong_sort.py
+++ strong_sort/strong_sort.py
@@ -45,8 +45,9 @@ class StrongSORT(object):
         # generate detections
         features = self._get_features(bbox_xywh, ori_img)
         bbox_tlwh = self._xywh_to_tlwh(bbox_xywh)
-        detections = [Detection(bbox_tlwh[i], conf, features[i]) for i, conf in enumerate(
-            confidences)]
+        # detections = [Detection(bbox_tlwh[i], conf, features[i]) for i, conf in enumerate(
+        #     confidences)]
+        detections = [Detection(bbox_tlwh[i], conf, features[i].detach()) for i, conf in enumerate(confidences)]
 
         # run on non-maximum supression
         boxes = np.array([d.tlwh for d in detections])
