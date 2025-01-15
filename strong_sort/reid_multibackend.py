471ac3c1b79cde3b2f9fb3eb9853f5ed55ae37ef
yxc407 <yuxiuchen407@gmail.com>
2025-01-15 11:33:40 +0800

    replace yolov5 with boxmot-compatible version

--- strong_sort/reid_multibackend.py
+++ strong_sort/reid_multibackend.py
@@ -164,7 +164,7 @@ class ReIDDetectMultiBackend(nn.Module):
         suffixes = list(export_formats().Suffix) + ['.xml']  # export suffixes
         check_suffix(p, suffixes)  # checks
         p = Path(p).name  # eliminate trailing separators
-        pt, jit, onnx, xml, engine, coreml, saved_model, pb, tflite, edgetpu, tfjs, _, xml2 = (s in p for s in suffixes)
+        pt, jit, onnx, xml, engine, coreml, saved_model, pb, tflite, edgetpu, tfjs, xml2 = (s in p for s in suffixes)
         xml |= xml2  # *_openvino_model or *.xml
         tflite &= not edgetpu  # *.tflite
         return pt, jit, onnx, xml, engine, coreml, saved_model, pb, tflite, edgetpu, tfjs
